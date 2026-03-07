a2b027f85021d98a68b1bafa5e8c4fcbc8bf98bc
FutureZhao <48658962+PeterKZhao@users.noreply.github.com>
2026-03-07 19:03:21 +0800

    Update __init__.py

--- crawler/sources/__init__.py
+++ crawler/sources/__init__.py
@@ -1,1 +1,11 @@
-undefined
+__all__ = [
+    "admissions",
+    "careers",
+    "budgets",
+    "recommendation",
+    "state_owned",
+    "civil_service",
+    "labs",
+    "industry",
+    "majors",
+]

5ea68a08c0342bd60e73560711763cd6827c2aaa
FutureZhao <48658962+PeterKZhao@users.noreply.github.com>
2026-03-07 18:25:32 +0800

    Update civil_service.py

---
+++ crawler/sources/civil_service.py
@@ -0,0 +1,10 @@
+SOURCES = [
+    {
+        "name": "公务员与选调生专题",
+        "url": "https://www.beijing.gov.cn/fuwu/bmfw/sy/jrts/202510/t20251014_4222424.html",
+        "topic": "civil_service",
+        "allow_domains": ["www.beijing.gov.cn", "beijing.gov.cn"],
+        "must_include": ["公务员", "报考指南", "职位", "专业", "选调生"],
+        "exclude_keywords": ["事业单位考试辅导"],
+    }
+]

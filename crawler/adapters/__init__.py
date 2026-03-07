5dc566d2f4e2d906902a7a5603b0b52da09058fe
FutureZhao <48658962+PeterKZhao@users.noreply.github.com>
2026-03-07 19:54:57 +0800

    Create __init__.py

---
+++ crawler/adapters/__init__.py
@@ -0,0 +1,7 @@
+from crawler.adapters.website import collect_from_website
+from crawler.adapters.xiaohongshu import collect_from_xiaohongshu
+
+__all__ = [
+    "collect_from_website",
+    "collect_from_xiaohongshu",
+]

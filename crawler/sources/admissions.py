32af503905c6da900bc0970e84a400bce349765b
FutureZhao <48658962+PeterKZhao@users.noreply.github.com>
2026-03-07 19:13:25 +0800

    Update admissions.py

--- crawler/sources/admissions.py
+++ crawler/sources/admissions.py
@@ -4,15 +4,37 @@ SOURCES = [
         "url": "https://zsb.snnu.edu.cn",
         "topic": "admissions",
         "allow_domains": ["zsb.snnu.edu.cn", "snnu.edu.cn"],
-        "must_include": ["招生", "本科", "录取", "分数", "计划", "专业"],
-        "exclude_keywords": ["研究生", "博士", "硕士"],
+        "must_include": [
+            "本科招生", "招生章程", "招生计划", "录取", "录取查询",
+            "历年分数", "分数线", "专业", "报考", "陕西"
+        ],
+        "exclude_keywords": [
+            "研究生", "博士", "硕士", "继续教育", "留学生"
+        ],
     },
     {
         "name": "西北大学本科招生信息网",
         "url": "https://zsb.nwu.edu.cn",
         "topic": "admissions",
         "allow_domains": ["zsb.nwu.edu.cn", "nwu.edu.cn"],
-        "must_include": ["招生", "本科", "录取", "分数", "计划", "章程"],
-        "exclude_keywords": ["研究生", "博士", "硕士"],
+        "must_include": [
+            "本科招生", "招生章程", "招生计划", "录取", "录取查询",
+            "历年分数", "分数线", "专业介绍", "报考"
+        ],
+        "exclude_keywords": [
+            "研究生", "博士", "硕士", "继续教育"
+        ],
+    },
+    {
+        "name": "西安邮电大学本科招生信息网",
+        "url": "https://zhaosheng.xupt.edu.cn/lqcx/fsfzycx/sx1.htm",
+        "topic": "admissions",
+        "allow_domains": ["zhaosheng.xupt.edu.cn", "xupt.edu.cn"],
+        "must_include": [
+            "陕西", "本科", "录取", "分数", "位次", "专业", "招生计划"
+        ],
+        "exclude_keywords": [
+            "研究生", "博士", "硕士"
+        ],
     },
 ]

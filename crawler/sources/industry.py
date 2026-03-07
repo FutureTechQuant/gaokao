d70c04db5acbfaea23432b2d8ce8b5a657f85d70
FutureZhao <48658962+PeterKZhao@users.noreply.github.com>
2026-03-07 18:25:52 +0800

    Update industry.py

---
+++ crawler/sources/industry.py
@@ -0,0 +1,10 @@
+SOURCES = [
+    {
+        "name": "产业链分析专题",
+        "url": "http://www.moe.gov.cn/",
+        "topic": "industry",
+        "allow_domains": ["www.moe.gov.cn", "moe.gov.cn"],
+        "must_include": ["产业", "人才培养", "新兴产业", "战略性新兴产业", "产业链"],
+        "exclude_keywords": ["中小学", "幼儿园"],
+    }
+]

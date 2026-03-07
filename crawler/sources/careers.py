428cacfffbac435080065a0d1ed678ee1fcbe33b
FutureZhao <48658962+PeterKZhao@users.noreply.github.com>
2026-03-07 18:24:54 +0800

    Update careers.py

---
+++ crawler/sources/careers.py
@@ -0,0 +1,18 @@
+SOURCES = [
+    {
+        "name": "陕西师范大学就业质量报告",
+        "url": "https://xxgk.snnu.edu.cn/gksx/jxzlxx/gxbysjyzlndbg.htm",
+        "topic": "careers",
+        "allow_domains": ["xxgk.snnu.edu.cn", "snnu.edu.cn"],
+        "must_include": ["就业质量", "毕业生就业", "就业去向", "行业分布", "升学"],
+        "exclude_keywords": ["研究生招生", "博士招生"],
+    },
+    {
+        "name": "西安理工大学就业质量报告",
+        "url": "https://xxgk.xaut.edu.cn/jxzlxx/gxbysjyzlndbg.htm",
+        "topic": "careers",
+        "allow_domains": ["xxgk.xaut.edu.cn", "xaut.edu.cn"],
+        "must_include": ["就业质量", "毕业生就业", "就业率", "地区流向"],
+        "exclude_keywords": ["研究生", "博士", "硕士"],
+    },
+]

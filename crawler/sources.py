d917f7dfe44f034efd22780a1a769215749ad57c
FutureZhao <48658962+PeterKZhao@users.noreply.github.com>
2026-03-07 12:05:14 +0800

    Update sources.py

--- crawler/sources.py
+++ crawler/sources.py
@@ -1,86 +1,143 @@
 SOURCES = [
     {
-        "name": "陕西省教育考试院",
-        "url": "https://www.sneea.cn",
+        "name": "教育部-2024年度本科专业备案和审批结果",
+        "url": "http://www.moe.gov.cn/srcsite/A08/moe_1034/s4930/202504/t20250422_1188239.html",
         "must_include": [
-            "高考", "普通高考", "招生", "录取", "志愿", "志愿填报",
-            "报名", "查分", "成绩查询", "分数线", "考试提醒", "政策"
+            "本科专业", "专业备案", "专业审批", "专业目录",
+            "新增专业", "撤销专业", "人工智能", "集成电路",
+            "智能制造", "储能", "数字经济"
         ],
         "exclude_keywords": [
-            "考研", "研究生", "硕士", "博士", "自考", "成考",
-            "四六级", "教师资格", "专升本", "留学"
+            "研究生", "博士", "硕士", "成人教育"
         ],
         "allow_domains": [
-            "www.sneea.cn", "sneea.cn"
+            "www.moe.gov.cn", "moe.gov.cn"
         ]
     },
     {
-        "name": "陕西招生考试信息网",
-        "url": "https://www.sneac.com",
+        "name": "教育部-2023年度本科专业备案和审批结果",
+        "url": "http://www.moe.gov.cn/srcsite/A08/moe_1034/s4930/202403/t20240319_1121111.html",
         "must_include": [
-            "高考", "普通高考", "招生", "录取", "志愿", "志愿填报",
-            "报名", "查分", "成绩查询", "分数线", "政策文件", "考试提醒", "历年数据"
+            "本科专业", "专业备案", "专业审批", "专业目录",
+            "新增专业", "交叉学科", "国家控制布点专业"
         ],
         "exclude_keywords": [
-            "考研", "研究生", "硕士", "博士", "自考", "成考",
-            "四六级", "教师资格", "专升本", "留学"
+            "研究生", "博士", "硕士"
         ],
         "allow_domains": [
-            "www.sneac.com", "sneac.com"
-        ]
-    },
-    {
-        "name": "陕西省教育厅-高考政策",
-        "url": "https://jyt.shaanxi.gov.cn/gk/zc/gfxwj_20255/gfxwj_20254/202501/t20250123_3440299.html",
-        "must_include": [
-            "高考", "普通高校招生", "招生", "录取", "考试安排",
-            "志愿填报", "成绩复核", "政策"
-        ],
-        "exclude_keywords": [
-            "考研", "研究生", "成考", "自考", "四六级", "教师资格"
-        ],
-        "allow_domains": [
-            "jyt.shaanxi.gov.cn"
-        ]
-    },
-    {
-        "name": "陕西省人民政府-高考新闻",
-        "url": "https://www.shaanxi.gov.cn/xw/sxyw/202506/t20250625_3535769.html",
-        "must_include": [
-            "高考", "成绩", "分数线", "志愿填报", "录取", "招生"
-        ],
-        "exclude_keywords": [
-            "考研", "研究生", "成考", "自考"
-        ],
-        "allow_domains": [
-            "www.shaanxi.gov.cn", "shaanxi.gov.cn"
+            "www.moe.gov.cn", "moe.gov.cn"
         ]
     },
     {
         "name": "陕西师范大学本科招生信息网",
         "url": "https://zsb.snnu.edu.cn",
         "must_include": [
-            "本科招生", "招生章程", "招生简章", "历年分数",
-            "录取查询", "高校专项", "高考"
+            "本科招生", "招生计划", "招生简章", "历年分数",
+            "录取查询", "高校专项", "专业", "高考"
         ],
         "exclude_keywords": [
-            "研究生", "硕士", "博士", "成人教育"
+            "研究生", "硕士", "博士", "继续教育"
         ],
         "allow_domains": [
             "zsb.snnu.edu.cn", "snnu.edu.cn"
         ]
     },
     {
-        "name": "西安邮电大学本科招生信息网",
-        "url": "https://zhaosheng.xupt.edu.cn/lqcx/fsfzycx/sx1.htm",
+        "name": "陕西师范大学-就业质量年度报告",
+        "url": "https://xxgk.snnu.edu.cn/gksx/jxzlxx/gxbysjyzlndbg.htm",
         "must_include": [
-            "陕西", "本科", "招生", "录取", "分数", "计划", "位次"
+            "就业质量年度报告", "就业质量", "毕业生就业",
+            "就业去向", "升学", "签约", "就业率", "行业分布"
+        ],
+        "exclude_keywords": [
+            "研究生招生", "博士招生", "硕士招生"
+        ],
+        "allow_domains": [
+            "xxgk.snnu.edu.cn", "snnu.edu.cn"
+        ]
+    },
+    {
+        "name": "西安理工大学-就业质量年度报告",
+        "url": "https://xxgk.xaut.edu.cn/jxzlxx/gxbysjyzlndbg.htm",
+        "must_include": [
+            "就业质量年度报告", "毕业生就业质量", "就业率",
+            "就业去向", "行业", "地区流向", "升学"
+        ],
+        "exclude_keywords": [
+            "研究生", "博士", "硕士"
+        ],
+        "allow_domains": [
+            "xxgk.xaut.edu.cn", "xaut.edu.cn"
+        ]
+    },
+    {
+        "name": "西北大学本科生招生信息网",
+        "url": "https://zsb.nwu.edu.cn",
+        "must_include": [
+            "本科招生", "招生章程", "招生计划", "历年分数",
+            "录取", "专业介绍", "高考"
         ],
         "exclude_keywords": [
             "研究生", "硕士", "博士"
         ],
         "allow_domains": [
+            "zsb.nwu.edu.cn", "nwu.edu.cn"
+        ]
+    },
+    {
+        "name": "西安邮电大学本科招生信息网-陕西分数",
+        "url": "https://zhaosheng.xupt.edu.cn/lqcx/fsfzycx/sx1.htm",
+        "must_include": [
+            "陕西", "本科", "招生", "录取", "分数",
+            "位次", "专业", "计划"
+        ],
+        "exclude_keywords": [
+            "研究生", "博士", "硕士"
+        ],
+        "allow_domains": [
             "zhaosheng.xupt.edu.cn", "xupt.edu.cn"
+        ]
+    },
+    {
+        "name": "西安邮电大学-就业质量相关",
+        "url": "https://zpb.xupt.edu.cn/info/1051/1047.htm",
+        "must_include": [
+            "就业质量", "毕业生就业", "月收入", "就业指数",
+            "陕西省", "本科高校", "就业"
+        ],
+        "exclude_keywords": [
+            "研究生", "博士", "硕士"
+        ],
+        "allow_domains": [
+            "zpb.xupt.edu.cn", "xupt.edu.cn"
+        ]
+    },
+    {
+        "name": "陕西理工大学招生就业网",
+        "url": "https://zsjyw.snut.edu.cn/zsw/index.htm",
+        "must_include": [
+            "招生信息", "招生政策", "招生计划", "历年",
+            "专业", "就业信息", "高考"
+        ],
+        "exclude_keywords": [
+            "研究生", "硕士", "博士"
+        ],
+        "allow_domains": [
+            "zsjyw.snut.edu.cn", "snut.edu.cn"
+        ]
+    },
+    {
+        "name": "陕西学前师范学院招生信息网",
+        "url": "https://zsw.snsy.edu.cn",
+        "must_include": [
+            "招生", "录取", "本科", "高考", "招生计划",
+            "录取快讯", "专业"
+        ],
+        "exclude_keywords": [
+            "研究生", "硕士", "博士", "继续教育"
+        ],
+        "allow_domains": [
+            "zsw.snsy.edu.cn", "snsy.edu.cn"
         ]
     }
 ]

3c0d0d8614f7c57927a0cfd3e61cce4e0e87a6cc
FutureZhao <48658962+PeterKZhao@users.noreply.github.com>
2026-03-07 18:26:00 +0800

    Update majors.py

---
+++ crawler/sources/majors.py
@@ -0,0 +1,10 @@
+SOURCES = [
+    {
+        "name": "教育部专业调整专题",
+        "url": "http://www.moe.gov.cn/srcsite/A08/moe_1034/s4930/202504/t20250422_1188239.html",
+        "topic": "majors",
+        "allow_domains": ["www.moe.gov.cn", "moe.gov.cn"],
+        "must_include": ["本科专业", "备案", "审批", "新增专业", "撤销专业"],
+        "exclude_keywords": ["研究生", "继续教育"],
+    }
+]

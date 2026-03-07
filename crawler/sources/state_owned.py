6a9979bda6f068f10a4fd6292f18a2a23fa3eac0
FutureZhao <48658962+PeterKZhao@users.noreply.github.com>
2026-03-07 18:25:23 +0800

    Update state_owned.py

---
+++ crawler/sources/state_owned.py
@@ -0,0 +1,10 @@
+SOURCES = [
+    {
+        "name": "国企招聘专题",
+        "url": "https://www.gzw.sh.gov.cn/",
+        "topic": "state_owned",
+        "allow_domains": ["www.gzw.sh.gov.cn", "gzw.sh.gov.cn"],
+        "must_include": ["国企", "招聘", "引才", "校招", "专业", "学历"],
+        "exclude_keywords": ["公务员", "事业单位"],
+    }
+]
